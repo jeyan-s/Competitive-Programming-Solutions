820df7043d2f201446c0a88db3249ee890708b70
Jeyan S <113619105+jeyan-s@users.noreply.github.com>
2023-11-19 18:54:33 +0530

    Update A-Rigged!.cpp

--- Codefores Solutions/2023-09-24-Educational Codeforces Round 155 (Rated for Div. 2)/A-Rigged!.cpp
+++ Codefores Solutions/2023-09-24-Educational Codeforces Round 155 (Rated for Div. 2)/A-Rigged!.cpp
@@ -1,1 +1,30 @@
+// Link to Problem Statement
+// https://codeforces.com/contest/1879/problem/A
 
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+int main() 
+{
+    int test_case; cin >> test_case;
+    while (test_case--)
+    {
+      // int n, k, a, b, rslt = 0; cin >> n;
+      // vector<int> lst(n);
+      // for (int x = 0; x < n; x++) cin >> lst[x];
+      int n, a, b, op = 0; cin >> n;
+      cin >> a >> b;
+      
+      for (int x = 1; x < n; x++)
+      {
+        int t, p;
+        cin >> t >> p;
+        if (t >= a) op = max(p, op);
+      }
+      if (b > op) cout << a << "\n";
+      else cout << -1 << "\n";
+      
+    }
+    return 0;
+}
