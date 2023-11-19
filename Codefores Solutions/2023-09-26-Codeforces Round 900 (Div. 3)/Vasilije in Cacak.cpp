3145fe2df9957b5fef8181c9d7c75aa07d19aa51
Jeyan S <113619105+jeyan-s@users.noreply.github.com>
2023-11-19 19:17:49 +0530

    Create Vasilije in Cacak.cpp

---
+++ Codefores Solutions/2023-09-26-Codeforces Round 900 (Div. 3)/Vasilije in Cacak.cpp
@@ -0,0 +1,23 @@
+Link to Problem Statement
+https://codeforces.com/contest/1878/problem/C
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+    int test; cin >> test;
+    while (test--)
+    {
+      long long n, k, x; cin >> n >> k >> x;
+      long long left = (k * (k + 1)) / 2;
+      long long new_k = n - k;
+      long long right = (new_k * (new_k + 1)) / 2;
+      right = ((n * (n + 1)) / 2) - right;
+      if (x >= left && x <= right)
+        cout << "YES\n";
+      else
+        cout << "NO\n";
+    }
+    return 0;
+}
